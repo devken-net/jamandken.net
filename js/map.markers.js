cdba87d0cc4b63aa8892cee6ec35ea6b2a75b900
marky <mark@techasiaportal.com>
2016-06-24 16:14:30 +0800

    firts commit

---
+++ js/map.markers.js
@@ -0,0 +1,47 @@
+//List of map markers
+var map_markers = [
+	{
+		"title": "Reception",
+		"latitude":14.59095,
+		"longitude":121.0736033,
+		"icon":"fa-glass", //Check the full list of icons at http://fortawesome.github.io/Font-Awesome/icons/
+		"infoWindow":"Valle Verde 6 Clubhouse <br> Pasig, Metro Manila"
+	},
+	{
+		"title": "Accommodation 1",
+		"latitude":14.5858117,
+		"longitude":121.0579821,
+		"icon":"fa-bed", //Check the full list of icons at http://fortawesome.github.io/Font-Awesome/icons/
+		"infoWindow":"Discovery Suites <br> Pasig, Metro Manila"
+	},
+	/*
+	{
+		"title": "Accommodation 2",
+		"latitude":33.780428,
+		"longitude":-118.084075,
+		"icon":"fa-bed", //Check the full list of icons at http://fortawesome.github.io/Font-Awesome/icons/
+		"infoWindow":"Accommodation 2 <br> Los Alamitos, CA 90720"
+	},
+	{
+		"title": "Accommodation 3",
+		"latitude":33.777551,
+		"longitude":-118.050468,
+		"icon":"fa-bed", //Check the full list of icons at http://fortawesome.github.io/Font-Awesome/icons/
+		"infoWindow":"Accommodation 3 <br> Seal Beach, CA 90740"
+	},
+	{
+		"title": "Transportation",
+		"latitude":33.782930,
+		"longitude":-118.060552,
+		"icon":"fa-plane", //Check the full list of icons at http://fortawesome.github.io/Font-Awesome/icons/
+		"infoWindow":"Seal Beach VORTAC SLI <br> Los Alamitos, CA 90720"
+	},
+	*/
+	{
+		"title": "Ceremony",
+		"latitude":14.5977097,
+		"longitude":121.0286337,
+		"icon":"fa-bell-o", //Check the full list of icons at http://fortawesome.github.io/Font-Awesome/icons/
+		"infoWindow":"Santuario del Santo Cristo <br> San Juan City, Metro Manila"
+	}
+];
